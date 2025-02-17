666a71677eb0214831b8030f6a195cd19e3fd0ad
Hayden <hg234@students.waikato.ac.nz>
2025-02-17 20:38:26 +1300

    add clear all embeds

--- main.py
+++ main.py
@@ -2,24 +2,34 @@ import os
 import discord
 from discord import app_commands
 from discord.ext import commands
-from dotenv.main import load_dotenv
+from dotenv import load_dotenv
 import json
+import random
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="*", intents=intents)
 type colorForm = list[int]
 load_dotenv()
+jsO = None
+def refreshJsonStore():
+    with open(os.path.dirname(os.path.realpath(__file__))+"/embeds.json",'r') as i:
+        jsO = json.load(i)
+    return jsO
+jsO = refreshJsonStore()
+def clearJsonStore():
+    with open(os.path.dirname(os.path.realpath(__file__))+"/embeds.json",'w') as o:
+        json.dump([], o)
+    jsO = refreshJsonStore()
+    return jsO
 
 #@bot.tree.command(name = "hello", description="Hi")
 #async def hello(interaction: discord.Interaction):
 #    await interaction.response.send_message("Test Output Hi")
 
-@bot.tree.command(name = "embed", description = "Embed a Message")
-async def embed(interaction: discord.Interaction, title:str, hexc:str, message_content:str, author:str=None):
-    jsO = None
-    with open('embeds.json', 'r') as i:
-        jsO = json.load(i)
+@bot.tree.command(name = "create_embed", description = "Embed a Message")
+async def create_embed(interaction: discord.Interaction, title:str, hexc:str, message_content:str, author:str=None):
+    jsO = refreshJsonStore()
     print(jsO)
 
     if author is None:
@@ -30,21 +40,66 @@ async def embed(interaction: discord.Interaction, title:str, hexc:str, message_c
     embed = discord.Embed(title=title, description=message_content, color=hex_int)
     embed.set_author(name=author)
     embDict = {
-        "title":title,
-        "hex_int":hex_int,
-        "author":str(author),
-        "content":message_content}
-    if embDict not in jsO:
+            "metadata":{
 
-jsO.append(embDict)
+                "channel_id":interaction.channel.id,
+                "message_id": ""
+            },
+            "embedinfo":{
+                "title":title,
+                "hex_int":hex_int,
+                "author":str(author),
+                "content":message_content
+                }
+            }
+    jsO, dosend = inpListIfNotMatch(embDict, jsO)
+    if dosend:
+        sent = await interaction.channel.send(embed=embed)
+        jsO[len(jsO)-1]["metadata"]["message_id"] = sent.id
+        await interaction.response.send_message("Embed Created successfully", ephemeral=True)
+    else:
+        await interaction.response.send_message("Same title and channel embed already exists", ephemeral=True)
 
-    with open('embeds.json', 'w') as o:
+
+
+
+
+
+    with open(os.path.dirname(os.path.realpath(__file__))+"/embeds.json", 'w') as o:
         json.dump(jsO, o)
-    await interaction.response.send_message(embed=embed)
-    await interaction.followup.send("Embed Created successfully", ephemeral=True)
 
 
 
+@bot.tree.command(name="delembed", description="delete an embed")
+async def delembed(interaction:discord.Interaction, message_id:int):
+
+    jsO = refreshJsonStore()
+    for i in list(jsO):
+        if i["metadata"]["message_id"] == message_id:
+            msg = await discord.channel.fetch_message(message_id)
+            await msg.delete()
+            jsO.remove(i)
+            await interaction.response.send_message("Message Deleted", ephemeral=True)
+
+@bot.tree.command(name="clearallembeds", description="Clear all embeds")
+async def clearallembeds(interaction:discord.Interaction):
+    clearJsonStore()
+    await interaction.response.send_message(f"All Embeds cleared. Current length of embed list is {len(jsO)}")
+
+def inpListIfNotMatch(d1:dict, ls:list):
+    for i in ls:
+        if (i["embedinfo"]["title"].lower() == d1["embedinfo"]["title"].lower()) and (i["metadata"]["channel_id"] == d1["metadata"]["channel_id"]):
+            print("matched")
+            return (ls, False)
+    ls.append(d1)
+    return (ls, True)
+def getUniqueID(ls:list):
+    j = 0
+    for i in ls:
+        j = random.randint(1000,9999)
+        if i["metadata"]["id"] == j:
+            return False
+    return j
 
 
 @bot.command()
@@ -58,8 +113,13 @@ async def sync(ctx):
 async def ping(ctx):
     await ctx.send("Ping")
 
+async def refreshEmbedsStore(ctx):
+    jsO=refreshJsonStore()
+    ctx.send(f"Refreshed {len(jsO)}")
 
-
+async def clearEmbedStore(ctx):
+    jsO = clearJsonStore()
+    ctx.send(f"Refreshed {len(jsO)}")
 
 
 
