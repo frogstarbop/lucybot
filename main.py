08da3ceed421c0295df4ce5dc5bd723edf6b0500
Hayden <hg234@students.waikato.ac.nz>
2025-02-19 18:09:37 +1300

    remove unused import

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
 import json
-import random
+
 
 intents = discord.Intents.default()
 intents.message_content = True
